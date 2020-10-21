861207416e8d9b77a73154d8f5e4bdcc8621334b
Shawn Wilson <wilson.ohyeah@gmail.com>
2020-10-20 20:17:35 -0600

    day 1

--- hackerrank/day1.js
+++ hackerrank/day1.js
@@ -23,15 +23,20 @@ function main() {
   var i = 4
   var d = 4.0
   var s = "HackerRank "
-  // Declare second integer, double, and String variables.
 
+  // Declare second integer, double, and String variables.
   // Read and save an integer, double, and String to your variables.
+  let i2 = parseInt(readLine());
+  let d2 = parseFloat(readLine());
+  let s2 = readLine();
 
   // Print the sum of both integer variables on a new line.
+  process.stdout.write(`${(i + i2).toString()}\n`);
 
   // Print the sum of the double variables on a new line.
+  process.stdout.write(`${(d + d2).toFixed(1).toString()}\n`);
 
   // Concatenate and print the String variables on a new line
   // The 's' variable above should be printed first.
-
+  process.stdout.write(`${s.concat(s2)}`);
 }
