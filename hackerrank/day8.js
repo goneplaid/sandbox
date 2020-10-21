3d04110b82029d1d767d9d37c9b8c0ee88a66cf2
Shawn Wilson <wilson.ohyeah@gmail.com>
2020-10-20 21:27:09 -0600

    day 8

--- hackerrank/day8.js
+++ hackerrank/day8.js
@@ -1,5 +1,27 @@
+function writeLine(string) {
+  return process.stdout.write(`${string}\n`);
+}
+
 function processData(input) {
-  //Enter your code here
+  const inputLines = input.split('\n');
+
+  const n = parseInt(inputLines.shift());
+
+  if (!(n >= 1 && n <= Math.pow(10, 5))) return;
+
+  const keyValuePairs = inputLines.slice(0, n);
+  const queries = inputLines.slice(n, inputLines.length + 1);
+  const phoneBook = new Map(
+    keyValuePairs.map(input => input.split(' '))
+  );
+
+  queries.forEach(element => {
+    if (phoneBook.has(element)) {
+      writeLine(`${element}=${phoneBook.get(element)}`);
+    } else {
+      writeLine('Not found');
+    }
+  });
 }
 
 process.stdin.resume();
