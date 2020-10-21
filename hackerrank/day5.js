6d7882b26c8c824c38a42c533c1ef9f19ad1dee5
Shawn Wilson <wilson.ohyeah@gmail.com>
2020-10-20 21:26:07 -0600

    day 5

--- hackerrank/day5.js
+++ hackerrank/day5.js
@@ -22,8 +22,18 @@ function readLine() {
   return inputString[currentLine++];
 }
 
-
+function writeLine(string) {
+  return process.stdout.write(`${string}\n`);
+}
 
 function main() {
   const n = parseInt(readLine(), 10);
+  const multipliers = Array.from(Array(10).keys());
+
+  if (n >= 2 && n <= 20) {
+    multipliers.forEach((value) => {
+      const multiplier = value + 1;
+      writeLine(`${n} x ${multiplier} = ${n * multiplier}`);
+    });
+  }
 }
