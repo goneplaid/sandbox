86f564c9021d703209611e182795734a7c6e28c0
Shawn Wilson <wilson.ohyeah@gmail.com>
2020-11-20 11:52:26 -0700

    Add some comments to the palindrome script

--- misc-exercises/palindromes.js
+++ misc-exercises/palindromes.js
@@ -1,11 +1,18 @@
 'use strict';
 
-// EXAMPLE
-// > npm run palindromes
-// accbzyzxilicbbbded
-// ----------
-// zidcbbabbcdiz
-// 13 chars long
+/*
+  Run the npm script and then type in an arbitrary number of characters
+  and press enter. The longest possible palindrome will be displayed along
+  with its length.
+
+  EXAMPLE
+
+  > npm run palindromes
+  > jalkjdsfljsdjllkjlkj8u9838&**^&*^&&^
+  | ----------
+  | sllkjjjd^8*&&*&&*8^djjjklls
+  | 27 chars long
+*/
 
 const _ = require('lodash');
 
@@ -31,6 +38,24 @@ function writeLine(value) {
   return process.stdout.write(`${value}\n`);
 }
 
+/*
+  This function takes an arbitrary length string and returns two structures,
+  a map and an array. The map contains a count for each character that can be
+  paired with another of its type. The array contains references of characters
+  that only appears once or in odd numbers.
+
+  For example, the input `aaccdiiaccdl` would return the following map of pair
+  counts and an array of single & odd numbered characters:
+
+  map: {
+    a: 1,
+    c: 2,
+    d: 1,
+    i: 1
+  }
+
+  array: [ a, l ]
+*/
 function sortChars(input) {
   const pairCounts = new Map();
   const discardedChars = [];
@@ -59,7 +84,18 @@ function sortChars(input) {
   }
 }
 
+/*
+  This function takes user input and displays the longest possible palindrome and
+  its length.
 
+  It sorts the characters in the string by counting the number of pairs that
+  occur for each one and also creates a list of unique chars or ones that occur in
+  odd numbers, which it uses for the mid-point of the palindrome (if one is found).
+
+  It then assembles half the palindrome, inserts a unique center character if one was
+  found, and mirrors the other half for first half of the full palindrome that is
+  displayed to the user.
+*/
 function buildPalindrome(input) {
   if (isPalindrome(input)) return input;
 
