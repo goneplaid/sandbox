dbc4206f462d10ed041c2f7d1118149f9b562274
Shawn Wilson <wilson.ohyeah@gmail.com>
2020-12-09 17:42:09 -0700

    Hide a few files that I never finished, submit a new problem solution for adding integers that exceed Number.MAX_SAFE_INTEGER

---
+++ misc-exercises/add-big-numbers.js
@@ -0,0 +1,74 @@
+'use strict';
+
+/*
+  This example problem requires that you add two very large integers together. Integers
+  that exceed Number.MAX_SAFE_INTEGER means that you need to manually add the numbers
+  together, digit by digit, and return the result as a string. 
+ 
+  EXAMPLE
+
+  > npm run add-big-numbers
+  > adding "999719925474492812" and "8349349989834983"
+  > equals "1008069275464327795"
+*/
+
+/**
+ * Because of the physical limitations on large numbers in JavaScript, they
+ * have to be handled as strings and added together similarly to how adding
+ * is taught to children in middle school.
+ * 
+ * Here we take two strings and we add up the characters in each sequence,
+ * from right to left, and add them up one by one, remembering to carry the
+ * one when appropriate :)
+ * 
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function (str1, str2) {
+  let string1 = str1;
+  let string2 = str2;
+
+  // Length values of our two strings
+  const length1 = string1.length;
+  const length2 = string2.length;
+
+  // An array of values collected during adding
+  let sumValues = [];
+  let carry = 0;
+
+  for (let i = 0; i < length1; i++) {
+    const a = string1.charAt(length1 - 1 - i);
+    const b = string2.charAt(length2 - 1 - i) || 0;
+    const sum = Number(a) + Number(b) + carry;
+
+    if (sum > 9) {
+      sumValues.push(sum.toString().charAt(1));
+      carry = 1;
+    } else {
+      sumValues.push(sum.toString());
+      carry = 0;
+    }
+
+    writeLine(`${a} + ${b} = ${sum}`);
+  }
+
+  if (carry) sumValues.push(carry.toString());
+
+  return sumValues.reverse().join('');
+};
+
+function writeLine(value) {
+  return process.stdout.write(`${value}\n`);
+}
+
+function main() {
+  const string1 = "999719925474492812";
+  const string2 = "8349349989834983";
+
+  writeLine(`adding numbers ${string1} and ${string2}`);
+  writeLine(`equals ${addStrings(string1, string2)}`);
+}
+
+main();
+
